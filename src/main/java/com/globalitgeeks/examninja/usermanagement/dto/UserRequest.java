45e2aad5bf33ffa93383e19778304ade66ed6643
shakibkhan0101 <shakibskhan01@gmail.com>
2024-09-21 19:54:25 +0530

    Added Backend API

---
+++ src/main/java/com/globalitgeeks/examninja/usermanagement/dto/UserRequest.java
@@ -0,0 +1,25 @@
+
+package com.globalitgeeks.examninja.usermanagement.dto;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class UserRequest {
+    @Email(message = "Email should be valid")
+    @NotBlank(message = "Email is mandatory")
+    private String email;
+    @NotBlank(message = "Password is required.")
+    @Size(min = 8, max = 15, message = "Password must be between 8 and 15 characters.")
+    @Pattern(regexp = ".*[!@#$%^&*()_+{}|:<>?].*", message = "Password must contain at least 1 special character.")
+    @Pattern(regexp = ".*[0-9].*", message = "Password must contain at least 1 number.")
+    private String password;
+}
+
---
+++ src/main/java/com/globalitgeeks/examninja/usermanagement/service/UserService.java
@@ -0,0 +1,62 @@
+package com.globalitgeeks.examninja.usermanagement.service;
+
+import com.globalitgeeks.examninja.usermanagement.dto.UserRegisterRequest;
+import com.globalitgeeks.examninja.usermanagement.dto.UserRequest;
+import com.globalitgeeks.examninja.usermanagement.exception.InvalidPasswordException;
+import com.globalitgeeks.examninja.usermanagement.exception.UserAlreadyExistsException;
+import com.globalitgeeks.examninja.usermanagement.exception.UserNotFoundException;
+import com.globalitgeeks.examninja.usermanagement.model.User;
+import com.globalitgeeks.examninja.usermanagement.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import java.util.Optional;
+
+@Service
+public class UserService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    // Register a new user
+    public User register(UserRegisterRequest registerRequest) {
+        Optional<User> userOptional = userRepository.findByEmail(registerRequest.getEmail());
+        if (userOptional.isPresent()) {
+            throw new UserAlreadyExistsException("User Already Exists with this Email!");
+        }
+        User user = new User();
+        user.setFirstName(registerRequest.getFirstName());
+        user.setLastName(registerRequest.getLastName());
+        user.setEmail(registerRequest.getEmail());
+        user.setPassword(registerRequest.getPassword());
+        return userRepository.save(user);
+    }
+
+    // Login user
+    public User login(UserRequest request) {
+        Optional<User> userOptional = userRepository.findByEmail(request.getEmail());
+
+        if (userOptional.isPresent()) {
+            User user = userOptional.get();
+            if (user.getPassword().equals(request.getPassword())) {
+                return user;
+            } else {
+                throw new InvalidPasswordException("Incorrect password");
+            }
+        } else {
+            throw new UserNotFoundException("User not found");
+        }
+    }
+
+    // Change user password
+    public User changePassword(UserRequest request) throws UserNotFoundException {
+        Optional<User> userOpt = userRepository.findByEmail(request.getEmail());
+        if (userOpt.isPresent()) {
+            User userPass = userOpt.get();
+            userPass.setPassword(request.getPassword());
+            return userRepository.save(userPass);
+        } else {
+            throw new UserNotFoundException("User not found with the provided email.");
+        }
+    }
+}
+
