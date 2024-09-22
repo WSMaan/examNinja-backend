a1205f3cf8376c9dbedab530ad48d16e6a345fa1
Shakib Khan <shakibskhan01@gmail.com>
2024-09-22 15:20:33 +0530

    added webconfig class related to frontend

---
+++ src/main/java/com/globalitgeeks/examninja/usermanagement/config/WebConfig.java
@@ -0,0 +1,17 @@
+package com.globalitgeeks.examninja.usermanagement.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")  // Allows CORS for all paths
+                .allowedOrigins("http://localhost:3000/")  // Your frontend URL
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")  // HTTP methods allowed
+                .allowedHeaders("*")  // All headers allowed
+                .allowCredentials(true);  // Allow credentials like cookies or authentication headers
+    }
+}
