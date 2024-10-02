c82ee268acbc643ac290189ab0f78529a02482a7
shamikkhn@gmail.com <Shamik@321>
2024-10-03 00:27:32 +0530

    Implemented ENJ-14, Fetching Question with the given test id in Get Request

--- src/main/java/com/globalitgeeks/examninja/service/QuestionService.java
+++ src/main/java/com/globalitgeeks/examninja/service/QuestionService.java
@@ -2,7 +2,6 @@ package com.globalitgeeks.examninja.service;
 
 import com.globalitgeeks.examninja.exception.ResourceNotFoundException;
 import com.globalitgeeks.examninja.model.Question;
-import com.globalitgeeks.examninja.model.TestTable;
 import com.globalitgeeks.examninja.repository.QuestionRepository;
 import com.globalitgeeks.examninja.repository.TestRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,8 +9,6 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
-
-import java.util.Optional;
 
 @Service
 public class QuestionService {
@@ -27,9 +24,14 @@ public class QuestionService {
        // Fetch one question per request based on the test id
         Pageable pageable = PageRequest.of(page, size);
         Page<Question> questions = questionRepository.findByTestId(testId, pageable);
-        if (questions.isEmpty()) {
+        if (questions.isEmpty() && page == 0) {
+            // If there are no questions at all, and it's the first page
             throw new ResourceNotFoundException("No questions found for test with Test Id: " + testId);
+        } else if (page > questions.getTotalPages() - 1) {
+            // If the page number requested exceeds the available pages
+            throw new IllegalArgumentException("Requested page is out of bounds. Maximum page number: " + (questions.getTotalPages() - 1));
         }
         return questions;
     }
+
 }
