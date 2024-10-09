32297b43a1c21cd4612e0a896ce3d433162f9760
shamikkhn@gmail.com <Shamik@321>
2024-10-10 00:40:50 +0530

    Implemented ENJ-14, Fetching Question with the given test id in Get Request. Updated - returning unique questions numbers for each test

--- src/main/java/com/globalitgeeks/examninja/controller/TestController.java
+++ src/main/java/com/globalitgeeks/examninja/controller/TestController.java
@@ -1,6 +1,6 @@
 package com.globalitgeeks.examninja.controller;
 
-<<<<<<< HEAD
+
 import com.globalitgeeks.examninja.service.QuestionService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -8,15 +8,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.Map;
 
-=======
-import com.globalitgeeks.examninja.model.Question;
-import com.globalitgeeks.examninja.service.QuestionService;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.domain.Page;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
 
->>>>>>> origin/ENJ-14
 @RestController
 @RequestMapping("/api/tests")
 public class TestController {
@@ -35,14 +27,6 @@ public class TestController {
         return ResponseEntity.ok(response);
     }
 }
-
-
-
-        // Fetch one question at a time based on the page number
-        Page<Question> question = questionService.getQuestionByTestId(testId, page, 1);
-        return ResponseEntity.ok(question);
-    }
->>>>>>> origin/ENJ-14
 
 
 
